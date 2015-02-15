b18b56aef6b279a2f2196c55d94a6d6327da7e47
kiwenlau <kiwenlau@126.com>
2015-02-15 16:48:38 +0900

    冒泡排序C语言版本，未优化

--- BubbleSort/main.c
+++ BubbleSort/main.c
@@ -7,9 +7,57 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//冒泡排序
+void bubble_sort(int *array, int N)
+{
+    int i, j, temp;
+    for (i=1; i<N; i++)
+    {
+        for (j=0; j<N-i; j++)
+        {
+            if (array[j]>array[j+1])
+            {
+                temp=array[j];   //交换
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    int *array, i, N=20;
+    array=(int*)malloc(N);
+    
+    //随机生成数组
+    srand((short)time(NULL));
+    for (i=0; i<N; i++)
+    {
+        array[i]=rand()%100;
+    }
+    
+    //输出原始数组
+    printf("Original array:\n");
+    for (i=0; i<N; i++)
+    {
+        printf("%2d  ", array[i]);
+    }
+    printf("\n\n");
+    
+    //调用冒泡排序
+    bubble_sort(array, N);
+    
+    //输出排序好的数组
+    printf("Sorted array:\n");
+    for (i=0; i<N; i++)
+    {
+        printf("%2d  ", array[i]);
+    }
+    printf("\n\n");
+    
     return 0;
 }
